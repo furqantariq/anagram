b96f9975eb5ce80c640634877d8e92f6148b681c
furqan <furqantrq@gmail.com>
2017-04-28 16:12:02 +0200

    keys for map in function

--- anagram/src/anagram/core.clj
+++ anagram/src/anagram/core.clj
@@ -1,6 +1,12 @@
 (ns anagram.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def prime [:a 2 :b 3 :c 5 :d 7 :e 11 :f 13 :g 17 :h 19])
+
+(update {8 ["abb"]} (reduce + (map {\a 2, \b 3} (seq "bab"))) conj "bab")
+
+(defn anagram
+  ([] 0)
+ ([x] (reduce + (map {\a 2, \b 3} (seq x)) )) 
+ ([x & more] (reduce + (map {\a 2, \b 3} (seq x)) ))
+ )
+
