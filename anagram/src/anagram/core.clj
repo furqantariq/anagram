b674445e2bbb88300474a7c5b75bebd866396e75
furqan <furqantrq@gmail.com>
2017-04-29 16:31:09 +0200

    working

--- anagram/src/anagram/core.clj
+++ anagram/src/anagram/core.clj
@@ -1,16 +1,17 @@
-
 (ns anagram.core)
 
-(def prime [:a 2 :b 3 :c 5 :d 7 :e 11 :f 13 :g 17 :h 19])
 
-(update {8 ["abb"]} (reduce + (map {\a 2, \b 3} (seq "bab"))) conj "bab")
-
+(let [alphaprime {\a 2, \b 3}]
 (defn anagram
- ([xy] (let [x (first xy)
+  ([xy result] (let [word (first xy)
              y (rest xy)]
-           (println  x )
-            (if (not-empty y) (anagram y))
-            )
-       )
-)
-      
+           (let [result (update result (reduce + (map alphaprime (seq word))) conj word)]
+           (println result)
+            (if (not-empty y) (anagram y result))
+            ))
+     )
+))
+
+ 
+ (anagram ["aa","bb","ab","ba"] {})
+ 
