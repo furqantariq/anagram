26ddba79e694ac7886a9e74e574a547e54f47d59
furqan <furqantrq@gmail.com>
2017-05-05 16:51:00 +0200

    final

--- anagram/src/anagram/core.clj
+++ anagram/src/anagram/core.clj
@@ -3,16 +3,24 @@
 
 
 
-(defn in_file 
+(defn input
   [file_name] (-> 
                 (slurp file_name :encoding "ISO-8859-1")
                 (split  #"\s+")
 ))
 
-(defn out_file 
-  [file_name data] (-> 
-                (spit file_name data :encoding "ISO-8859-1")
-))
+(defn output [data]   
+  (spit "output.txt" (println-str "WORDS    ANAGRAM") :encoding "ISO-8859-1")
+  (doseq [x data]
+    (let [w (first x)
+          r (rest x)]
+    (spit "output.txt" (println-str w "     " r) :encoding "ISO-8859-1" :append true)
+    ))
+  )
+(defn output_stat
+  [tag data]   (spit "output.txt" 
+                     (println-str tag " : " data ) :encoding "ISO-8859-1" :append true)
+  )
 
 
 (defn anagram [xy result]
@@ -22,10 +30,20 @@
    (if (not-empty y)
       (recur y r2)
       r2)))
- 
-(anagram ["aa","bb","ab","ba"] {})
-(out_file "C:\\Users\\furqa\\Downloads\\eventyr.txt")
-(println (join \newline (vals (anagram ["aa","bb","aB","ba"] {}))))
-(- (reduce + (map count (vals (anagram ["aa","bb","aB","ba"] {})))) (count (keys (anagram ["aa","bb","aB","ba"] {}))))
+
+
+(defn find-anagram [file]
+  (let [result (anagram (input file) {})
+        all_key (keys result)
+        all_val (vals result)]
+    (output all_val)
+    (output_stat "Total Anagram" (- (reduce + (map count all_val)) (count (keys all_key))))
+    (output_stat "Words with Anagram" (count (filter #(> (count %) 1) all_val)))
+    (output_stat "Words without Anagram" (count (filter #(= (count %) 1) all_val)))
+
+    )
+  )
+
+(find-anagram "C:\\Users\\furqa\\Downloads\\eventyr.txt")
 
 
