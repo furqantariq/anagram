1b6f0ef257ee1f87891110b3b86c5cd34d73480d
furqan <furqantrq@gmail.com>
2017-05-05 14:48:46 +0200

    working in different units

--- anagram/src/anagram/core.clj
+++ anagram/src/anagram/core.clj
@@ -1,25 +1,31 @@
 (ns anagram.core)
-(use '[clojure.string :only (split lower-case)])
+(use '[clojure.string :only (split lower-case join)])
 
 
 
-(defn read_file 
+(defn in_file 
   [file_name] (-> 
                 (slurp file_name :encoding "ISO-8859-1")
-                (lower-case)
                 (split  #"\s+")
 ))
+
+(defn out_file 
+  [file_name data] (-> 
+                (spit file_name data :encoding "ISO-8859-1")
+))
+
 
 (defn anagram [xy result]
   (let [word (first xy)
         y (rest xy)
-        r2 (update result (apply str (sort word)) conj word)]
-    (println result)
-    (if (not-empty y)
+        r2 (update result (apply str (sort (lower-case word))) conj word)]
+   (if (not-empty y)
       (recur y r2)
       r2)))
  
- (anagram ["aa","bb","ab","ba"] {})
-(read_file "C:\\Users\\furqa\\Downloads\\eventyr.txt")
+(anagram ["aa","bb","ab","ba"] {})
+(out_file "C:\\Users\\furqa\\Downloads\\eventyr.txt")
+(println (join \newline (vals (anagram ["aa","bb","aB","ba"] {}))))
+(- (reduce + (map count (vals (anagram ["aa","bb","aB","ba"] {})))) (count (keys (anagram ["aa","bb","aB","ba"] {}))))
 
- 
+
