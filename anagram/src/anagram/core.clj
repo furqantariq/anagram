8e47151b27c24622f8a4ceb57584b2838c3a6d7d
furqan <furqantrq@gmail.com>
2017-05-05 14:01:23 +0200

    output function remaining

--- anagram/src/anagram/core.clj
+++ anagram/src/anagram/core.clj
@@ -1,17 +1,25 @@
 (ns anagram.core)
+(use '[clojure.string :only (split lower-case)])
 
 
-(let [alphaprime {\a 2, \b 3}]
-(defn anagram
-  ([xy result] (let [word (first xy)
-             y (rest xy)]
-           (let [result (update result (reduce + (map alphaprime (seq word))) conj word)]
-           (println result)
-            (if (not-empty y) (anagram y result))
-            ))
-     )
+
+(defn read_file 
+  [file_name] (-> 
+                (slurp file_name :encoding "ISO-8859-1")
+                (lower-case)
+                (split  #"\s+")
 ))
 
+(defn anagram [xy result]
+  (let [word (first xy)
+        y (rest xy)
+        r2 (update result (apply str (sort word)) conj word)]
+    (println result)
+    (if (not-empty y)
+      (recur y r2)
+      r2)))
  
  (anagram ["aa","bb","ab","ba"] {})
+(read_file "C:\\Users\\furqa\\Downloads\\eventyr.txt")
+
  
